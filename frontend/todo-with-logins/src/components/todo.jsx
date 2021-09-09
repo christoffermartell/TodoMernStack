ae3827814a77d38e2cbff2dc5b98a55815da2ff6
Nikola Maljenovic <32510744+NikolaMC@users.noreply.github.com>
2021-09-09 02:20:00 +0200

    Created 3 new components and moved parts of the html code from allTodos there

--- frontend/todo-with-logins/src/components/todo.jsx
+++ frontend/todo-with-logins/src/components/todo.jsx
@@ -1,19 +1,45 @@
 import React from "react";
 
-const Todo = (props) => {
-	const todo = props.todo;
-
+const Todo = ({ posts, deleteTodo, openModal, handleUpdateFinished }) => {
 	return (
-		<h3 className="text-dark text-center p-1 bg-light border-bottom">
-			<i
-				className="far fa-times-circle fa-sm  m-1 text-danger float-start"
-				onClick={() => props.deleteTodo(todo)}
-			></i>
-			{todo}
-
-			<input type="checkbox" className="m-2 float-end"></input>
-		</h3>
-	);
+        <div
+            key={posts._id}
+            style={{
+                textDecoration: posts.completed
+                    ? "line-through"
+                    : "",
+            }}
+        >
+            <h4 className="text-dark text-center p-1 bg-light border-bottom ">
+                {posts.title}
+                <i
+                    className="far fa-times-circle fa-sm m-1 text-danger float-start"
+                    onClick={() => deleteTodo(posts._id)}
+                ></i>
+                <i
+                    className="fa fa-pencil-square-o fa-sm m-1 float-end"
+                    aria-hidden="true"
+                    onClick={() =>
+                        openModal(
+                            posts.title,
+                            posts.content,
+                            posts._id
+                        )
+                    }
+                ></i>
+                <input
+                    defaultChecked={posts.completed}
+                    onChange={(e) =>
+                        handleUpdateFinished(e, posts._id)
+                    }
+                    type="checkbox"
+                    className="m-2 float-end"
+                ></input>
+            </h4>
+            <p>{posts.content}</p>
+            <p>{posts.completed}</p>
+        </div>
+    );
 };
 
 export default Todo;
